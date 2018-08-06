b894c49b3663aec55752adaa37686377e4e1fce5
Zekotsu <36639546+Zekotsu@users.noreply.github.com>
2018-08-06 17:46:38 +0200

    Import 1
    Searched for how to implement the import of JPG/PNJ
    Can read an image from online but not from local
    Once local read is complete, it will need UI for file path search

---
+++ Visual Presentation/Assets/Scripts/ImageRetriever.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+
+//Open a picture from the internet, not from the computer yet
+public class ImageRetriever : MonoBehaviour {
+
+	string filePath = "C:\\Users\\Quentin\\Desktop\\Quentin\\images\\Fonds d'écran";
+	Texture2D img;
+
+	// Use this for initialization
+	void Start () {
+		img = LoadPNG (filePath);
+		//StartCoroutine (LoadImg());
+	}
+
+	//IEnumerator LoadImg()
+	//{
+	//	yield return 0;
+	//	WWW imgLink = new WWW (filePath);
+	//	yield return imgLink;
+	//	img = imgLink.texture;
+	//}
+
+	// Update is called once per frame
+	void OnGUI () {
+		GUILayout.Label (img);
+	}
+
+	public static Texture2D LoadPNG(string filePath) {
+
+		Texture2D tex = null;
+		byte[] fileData;
+
+		if (File.Exists (filePath)) {
+			Debug.Log ("Le fichier est trouvé");
+			fileData = File.ReadAllBytes (filePath);
+			tex = new Texture2D (2, 2);
+			tex.LoadImage (fileData); //..this will auto-resize the texture dimensions.
+		}
+		else 
+		{
+			Debug.Log ("Nope");
+		}
+		return tex;
+	}
+
+}

b2ef9fe754a15608341bfa64c3880f1c36c486fd
Zekotsu <36639546+Zekotsu@users.noreply.github.com>
2018-08-28 19:09:46 +0200

    Import 7 + Camera problem 1
    User can choose between all existing presentation to edit.
    
    UI rotate and zoom button no longer operationnal!

---
+++ Visual Presentation/Assets/Scripts/CanvasChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasChange : MonoBehaviour {
+
+	[SerializeField] Canvas canvasToActivate;
+	[SerializeField] Canvas currentCanvas;
+
+	[SerializeField] bool isThereInitialy;
+
+	void Start () {
+		if (isThereInitialy) {
+			canvasToActivate.enabled = false;
+		}
+	}
+
+	public void Escape () {
+		canvasToActivate.enabled = true;
+		currentCanvas.enabled = false;
+	}
+}

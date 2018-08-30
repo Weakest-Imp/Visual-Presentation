3bcaf502ddb449c748d3aa0151b2ea55bf550e2c
Zekotsu <36639546+Zekotsu@users.noreply.github.com>
2018-08-30 12:27:36 +0200

    Import 8
    Can get file path from user.
    It took 1 line...

---
+++ Visual Presentation/Assets/Scripts/Main Menu/ObtainFilePath.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ObtainFilePath : MonoBehaviour {
+
+	public void ObtainPath () {
+		GameManager.Instance.SetFilePath(
+			EditorUtility.OpenFilePanel ("Select file", "", ""));
+		Debug.Log (GameManager.Instance.filePath);
+	}
+}
